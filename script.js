cf35d7527208fb6f7a879fe131e257e9e9adf695
triciatriza <destrizapatricia@gmail.com>
2022-06-16 09:56:30 +0800

    added audio and horn button function

--- script.js
+++ script.js
@@ -25,55 +25,70 @@ const minusBtn = document.getElementById("minusbtn")
 const periodValue = document.getElementById("currentPeriod")
 let periodCounter = 0
 
+const hornBtn = document.getElementById("horn-btn")
 
 
-homePlusOneEl.addEventListener("click", function(){
+
+homePlusOneEl.addEventListener("click", function () {
     homeCount = homeCount + 1
     homeScoreEl.textContent = homeCount
 })
 
-homePlusTwoEl.addEventListener("click", function(){
+homePlusTwoEl.addEventListener("click", function () {
     homeCount = homeCount + 2
     homeScoreEl.textContent = homeCount
 })
 
-homePlusThreeEl.addEventListener("click", function(){
+homePlusThreeEl.addEventListener("click", function () {
     homeCount = homeCount + 3
     homeScoreEl.textContent = homeCount
 })
 
+//timer
+let timerEl = document.getElementById("currentTimer") //time displayed
+let minuteEl = document.getElementById("editMinutes").value //minutes
+let secondEl = document.getElementById("editSeconds").value //seconds
+const setTimer = document.getElementById("setTimer-btn")
+const startBtn = document.getElementById("start-btn")
+const stopBtn = document.getElementById("stop-btn")
+const resetBtn = document.getElementById("reset-btn")
+
+setTimer.addEventListener("click", function () {
+    let minuteEl = document.getElementById("editMinutes").value //minutes
+    let secondEl = document.getElementById("editSeconds").value //seconds
+    timerEl.innerHTML = `${minuteEl}:${secondEl}`
+})
 
 
-guestPlusOneEl.addEventListener("click", function(){
+
+guestPlusOneEl.addEventListener("click", function () {
     guestCount = guestCount + 1
     guestScoreEl.textContent = guestCount
 })
 
-guestPlusTwoEl.addEventListener("click", function(){
+guestPlusTwoEl.addEventListener("click", function () {
     guestCount = guestCount + 2
     guestScoreEl.textContent = guestCount
 })
-guestPlusThreeEl.addEventListener("click", function(){
+guestPlusThreeEl.addEventListener("click", function () {
     guestCount = guestCount + 3
     guestScoreEl.textContent = guestCount
 })
 
 
 
-homeFoulBtn.addEventListener("click", function(){
+homeFoulBtn.addEventListener("click", function () {
     homeFoulCount = homeFoulCount + 1
     homeFoulEl.textContent = homeFoulCount
 })
 
-guestFoulBtn.addEventListener("click", function(){
+guestFoulBtn.addEventListener("click", function () {
     guestFoulCount = guestFoulCount + 1
     guestFoulEl.textContent = guestFoulCount
 })
 
-//TODO: timer here
-
 //resets all current values
-newGameBtn.addEventListener("click", function(){
+newGameBtn.addEventListener("click", function () {
     count = 0
     homeScoreEl.textContent = count
     guestScoreEl.textContent = count
@@ -84,12 +99,18 @@ newGameBtn.addEventListener("click", function(){
 
 //period should only be 1-4
 //TODO: put if-else condition coz it goes beyond negative values when value is decreased lol
-plusBtn.addEventListener("click", function(){
+plusBtn.addEventListener("click", function () {
     periodCounter = periodCounter + 1
     periodValue.textContent = periodCounter
 })
 
-minusBtn.addEventListener("click", function(){
+minusBtn.addEventListener("click", function () {
     periodCounter = periodCounter - 1
     periodValue.textContent = periodCounter
+})
+
+hornBtn.addEventListener("click", function(){
+    const audio = new Audio()
+    audio.src = "./buzzer.mp3"
+    audio.play()
 })
